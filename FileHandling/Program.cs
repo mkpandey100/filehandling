d3e35e9ccca00ded43751b2632629f14ee8574bb
Mahendra Pandey <mpandey@qkly.io>
2022-12-12 13:28:34 +0545

    initial commit

---
+++ FileHandling/Program.cs
@@ -0,0 +1,55 @@
+﻿
+using System.Text;
+
+namespace FileHandling
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = "D:\\csharpfile.txt";
+
+            //CreateFile(path);
+            //WritingTextFile(path);
+            //AppendFileText(path);
+            //OpenFile(path);
+            //TruncateFile(path);
+            DeleteFile(path);
+        }
+
+        private static void CreateFile(string path)
+        {
+            FileStream fs = new(path, FileMode.Create);
+            fs.Close();
+        }
+        private static void WritingTextFile(string path)
+        {
+            FileStream fs = new(path, FileMode.Create);
+            byte[] bdata = Encoding.Default.GetBytes("Hello File Handling!");
+            fs.Write(bdata, 0, bdata.Length);
+            fs.Close();
+        }
+
+        private static void OpenFile(string path)
+        {
+            File.Open(path, FileMode.Open, FileAccess.Read, FileShare.None);
+            //File.OpenRead(path);
+            Console.ReadKey();
+        }
+        private static void AppendFileText(string path)
+        {
+            File.AppendAllText(path, "Here is new text content new" + Environment.NewLine);
+        }
+
+        private static void TruncateFile(string path)
+        {
+            FileStream fs = new FileStream(path, FileMode.Open);
+            fs.SetLength(10);
+        }
+
+        private static void DeleteFile(string path)
+        {
+            File.Delete(path);
+        }
+    }
+}
